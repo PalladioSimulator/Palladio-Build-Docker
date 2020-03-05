ef7e1b457c556d4e6c192e7d7fb0e6520a9c1922
He Zhentang <he.zhentang@qq.com>
2020-03-05 16:22:10 +0100

    Update RunExperimentAutomation.sh

--- PalladioExperimentAutomation/RunExperimentAutomation.sh
+++ PalladioExperimentAutomation/RunExperimentAutomation.sh
@@ -3,7 +3,7 @@
 Xvfb :99 -screen 0 1920x1080x16 &
 export DISPLAY=:99
 /usr/ModifyExperimentsFile.sh "$1" "$2"
-/wksp_eclipse/eclipse/eclipse \
+/result/eclipse/eclipse \
      -clean \
      -application org.palladiosimulator.experimentautomation.application \
      -consoleLog "$2" \
